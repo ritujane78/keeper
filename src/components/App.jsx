d55114b5fd69f781d67315da9b7d3fb176a0bd4d
Ritu Bafna <ritujane78@gmail.com>
2025-02-13 17:57:09 +0545

    add and delete functionalities

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
-import Notes from "./Notes";
 import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./NewNote";
 
-function App(){
-    return(
-        <div>
-            <Header />
-            <Notes />
-            <Footer />
-        </div>
-        
-    );
+
+function App() {
+    const [allNotes, setAllNotes]=useState([])
+    function addNote(note){
+        setAllNotes(prevNotes => {
+            return [
+                ...prevNotes,
+                 note
+            ]
+    }
+)
+
+    }
+function deleteNote(id){
+    setAllNotes(prevNotes => prevNotes.filter((note, index) => index !== id))
 }
+  return (
+    <div>
+      <Header />
+      <CreateArea addAllNotes = {addNote} />
+      {allNotes.map((note, index) => {
+      return (<Note
+                key={index}
+                id={index} 
+                title={note.title} 
+                content={note.content} 
+                toDelete={deleteNote} 
+            />);
+      })}
+      <Footer />
+    </div>
+  );
+}
+
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 
 function Note(props){
+    function deleteNote(){
+        props.toDelete(props.id);
+    }
     return(
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={deleteNote}>Delete</button>
         </div>
     );
 }
