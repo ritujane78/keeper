86a4acaf789f478b89e9664cc74adbef77112b6c
Ritu Bafna <ritujane78@gmail.com>
2025-02-15 13:38:20 +0545

    correct footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ Ritu Bafna {year}</p>
+      <p>Copyright ⓒ {year} Ritu Bafna</p>
     </footer>
   );
 }
